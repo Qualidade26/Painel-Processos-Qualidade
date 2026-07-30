f9e0ff657aec85037e69cd560dfc420ed9be5e69
Qualidade26 <qualidade@laborhs.com.br>
2026-07-30 08:54:15 -0300

    Update esfig.js

--- js/esfig.js
+++ js/esfig.js
@@ -43,6 +43,7 @@ function renderEsfig(){
     | TOTAIS DO FLUXO OPERACIONAL
     |--------------------------------------------------------------------------
     */
+   
     const totalAguardando =
         produtos.reduce(
             (soma,item) =>
